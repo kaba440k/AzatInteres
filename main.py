c42720686c0d0796e9e85e5774dbfdefd1de0a4f
Дмитрий <dimasvidlov05@gmail.com>
2021-12-16 23:28:08 +0300

    last version

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ import time
 def main():
     l_minimal = int(input())
     r_maximum = int(input())
-    start_time = time.time()
-    all_roots = []
     if l_minimal <= r_maximum:
+        all_roots = []
+        start_time = time.time()
         for i in range(l_minimal, r_maximum + 1):
-            if i == -1:
-                all_roots.clear()
-                print(-1)
-                break
-            else:
-                interval = [100000, -100000 + i]
-                while interval[1] != 100000:
-                    if discriminant(interval[0], interval[1]) is not None:
-                        all_roots.append(discriminant(interval[0], interval[1]))
-                    interval[0] -= 1
-                    interval[1] += 1
-        if len(all_roots) != 0:
-            print(all_roots, "\n", len(all_roots))
+            max_interal = 100000
+            min_interval = -100000 + i
+            interval = [max_interal, min_interval]
+            while interval[1] != max_interal:
+                now_disc = discriminant(interval[0], interval[1])
+                if now_disc is not None:
+                    all_roots.append(now_disc)
+                interval[0] -= 1
+                interval[1] += 1
+        print(all_roots, "\n", len(all_roots))
         print("--- %s seconds ---" % (time.time() - start_time))
     else:
         print("l_minimal > r_maximum")
@@ -28,16 +25,13 @@ def main():
 
 def discriminant(b_coef, c_coef):
     disc = b_coef ** 2 - 4 * c_coef
-    roots = [0, 0]
-    if disc > 0:
-        roots[0] = (b_coef * -1 + disc ** 0.5) / 2
-        roots[1] = (b_coef * -1 - disc ** 0.5) / 2
-        if roots[0] % 1 == 0 and roots[1] % 1 == 0:
-            return roots
-        else:
-            return None
-    else:
-        return None
+    if disc >= 0:
+        root_of_disc = disc ** 0.5
+        x1 = (-b_coef + root_of_disc) / 2
+        x2 = (-b_coef - root_of_disc) / 2
+        if x1 % 1 == 0 and x1 == x2 or x2 % 1 == 0:
+            return x1, x2
+    return None
 
 
 if __name__ == "__main__":
